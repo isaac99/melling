66b99c48dcbec917c12f90ea1390640dc070550b
isaac.shiffman <isaac.shiffman@kairostech.no>
2023-06-28 02:21:47 +0200

    some changes

--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,7 +1,7 @@
 export const colors = {
-  background: "#f03e2f",
+  background: "#e2ded0",
   text: "#000000",
-  primary: "#000000",
+  primary: "#ab8940",
   muted: "#f14e41",
   active: "#e6e6e6",
   black: "#000",
--- src/components/hero.js
+++ src/components/hero.js
@@ -13,28 +13,36 @@ import {
   Text,
 } from "./ui"
 
+import * as styles from "./hero.css"
+
 export default function Hero(props) {
   return (
-    <Section>
-      <Container>
-        <Flex gap={4} variant="responsive">
-          <Box width="half">
+    <Section className={styles.heroSection}>
+      <Container className={styles.heroImageContainer}>
+        <Flex gap={4} variant="responsive" className={styles.heroFlex}>
+          <Box width="full">
             {props.image && (
               <GatsbyImage
                 alt={props.image.alt}
                 image={getImage(props.image.gatsbyImageData)}
               />
+              
             )}
           </Box>
-          <Box width="half">
-            <Heading as="h1">
-              {props.kicker && <Kicker>{props.kicker}</Kicker>}
-              {props.h1}
-            </Heading>
-            <Subhead as="h2">{props.subhead}</Subhead>
-            <Text as="p">{props.text}</Text>
-            <ButtonList links={props.links} />
-          </Box>
+
+            {/* <Box width="half"> */}
+
+              <div className={styles.heroHeader}>
+                <Heading as="h1">
+                  {props.kicker && <Kicker>{props.kicker}</Kicker>}
+                  {props.h1}
+                </Heading>
+                <Subhead as="h2">{props.subhead}</Subhead>
+                <Text as="p">{props.text}</Text>
+                <ButtonList links={props.links} />
+              </div>
+              {/* </Box> */}
+
         </Flex>
       </Container>
     </Section>
